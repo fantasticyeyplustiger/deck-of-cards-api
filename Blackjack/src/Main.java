014f951479e7e8b94544515298d2568675cb30b8
fantasticyeyplustiger <143634964+fantasticyeyplustiger@users.noreply.github.com>
2024-11-05 12:40:12 -0500

    new code + new build

--- Blackjack/src/Main.java
+++ Blackjack/src/Main.java
@@ -1,31 +1,45 @@
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static String[] shuffledDeckValues = new String[4];
+    public static String deckID;
+    public static int remainingCards;
     public static double money = 250.0;
     public static double betting_power = 1;
 
     public static void main(String[] args) {
 
-        System.out.println("start gambling buddy");
+        System.out.println("start gambling buddy\n");
 
-        Scanner scan = new Scanner(shuffleDeck());
+        Scanner scan = new Scanner(getShuffledDeck());
         String shuffledDeck = scan.useDelimiter("\\A").next();
         shuffledDeck = removeUnnecessaryLetters(shuffledDeck);
 
-        System.out.println(shuffledDeck);
+        setShuffledDeckValues(shuffledDeck);
+
+        Scanner scann = new Scanner(getCard("1"));
+        System.out.println(scann.useDelimiter("\\A").next());
     }
 
-    private static String setShuffledDeckValues(String value){
-        //get a specific value from a string in java tab
+    private static void setShuffledDeckValues(String value){
+
+        //looks at "value" as if it is a hash map
+        Pattern pattern = Pattern.compile("(\\w+?):(\\w+?),");
+        Matcher matcher = pattern.matcher(value);
+
+        while(matcher.find()){
+            switch (matcher.group(1)){ // the "key"
+                case "deck_id" -> deckID = matcher.group(2); // the "value"
+                case "remaining" -> remainingCards = Integer.parseInt(matcher.group(2));
+            }
+        }
     }
 
     /**
@@ -37,8 +51,8 @@ public class Main {
         StringBuilder returnedString = new StringBuilder();
 
         for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == ' ' || string.charAt(i) == '"' || string.charAt(i) == '{'
-        || string.charAt(i) == '}') {
+            char letter = string.charAt(i);
+            if (letter == ' ' || letter == '"' || letter == '{' || letter == '}') {
                 continue;
             }
             returnedString.append(string.charAt(i));
@@ -47,10 +61,11 @@ public class Main {
         return String.valueOf(returnedString);
     }
 
-    public static InputStream shuffleDeck() {
+    private static InputStream getCard(String amountOfCards){
         try {
-            URLConnection url = new URL("https://www.deckofcardsapi.com/" +
-                    "api/deck/new/shuffle/?deck_count=1").openConnection();
+            URLConnection url = new URL
+                    ("https://www.deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=" + amountOfCards)
+                    .openConnection();
 
             return url.getInputStream();
 
@@ -59,49 +74,15 @@ public class Main {
         }
     }
 
-    public static String executePost(String targetURL, String urlParameters) {
-        HttpURLConnection connection = null;
-
+    public static InputStream getShuffledDeck() {
         try {
-            //Create connection
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type",
-                    "application/x-www-form-urlencoded");
+            URLConnection url = new URL("https://www.deckofcardsapi.com/" +
+                    "api/deck/new/shuffle/?deck_count=1").openConnection();
 
-            connection.setRequestProperty("Content-Length",
-                    Integer.toString(urlParameters.getBytes().length));
-            connection.setRequestProperty("Content-Language", "en-US");
+            return url.getInputStream();
 
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            //Send request
-            DataOutputStream wr = new DataOutputStream(
-                    connection.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.close();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            return response.toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
